f77bf9d2221c8f87c8e635f9a77d34a505aa40af
Simo Kivimäki <simo@kivimaki.fi>
2014-11-02 15:26:03 +0200

    Fixed parsing for new simponit.org layout.

--- simpsonitorgparser.py
+++ simpsonitorgparser.py
@@ -56,10 +56,9 @@ def parseSchedule():
 # Hakee Suomessa esitettyjen Simpsonien aikataulut valitulta vuodelta
 def parseYear(session, year):
     schedules = []
-    params = {'vuosi': year}
-    url = 'http://simpsonit.org/?sivu=esitysaikatauluarkisto'
-    soup = BeautifulSoup(session.get(url, params=params).content)
-    for listing_table in soup.find_all('table', class_='listing'):
+    url = 'http://simpsonit.org/jaksot/tv-esitykset-suomessa/' + str(year)
+    soup = BeautifulSoup(session.get(url).content)
+    for listing_table in soup.find_all('table', class_='tv-archive'):
         for tr in listing_table.find_all('tr'):
             schedules.extend(handle_schedule_tr(tr))
     print 'Simpsonit.org: Found', len(schedules), 'schedules for year', year
@@ -73,11 +72,10 @@ def handle_schedule_tr(tr):
     for schedule_b in tds[1].find_all('b'):
         time_string = schedule_b.string[:5]
         dt = datetime.strptime(date_string + ' ' + time_string, "%d.%m.%Y %H:%M")
-        season_episode_name = list(schedule_b.find_next_sibling('a').stripped_strings)
-        #print "season_episode_name", season_episode_name
-        season_episode = season_episode_name[0][:-2]
-        name = season_episode_name[1] if len(season_episode_name) > 1 else None
-        m = re.match(r'(\d+)x(\d+)', season_episode)
+        episode_a = schedule_b.find_next_sibling('a')
+        episode_url = episode_a.get('href')
+        name = episode_a.string
+        m = re.match(r'/jaksot/(\d+)/(\d+)/', episode_url)
         season = int(m.group(1))
         episode = int(m.group(2))
         #print dt, season, episode, name
