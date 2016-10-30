c0b0e1f652f29cbbb88ffca8d8c8f331b618f8b2
kattelus <mataka@iki.fi>
2016-10-30 17:43:48 +0200

    pybuilder and installable scripts etc

--- build.py
+++ build.py
@@ -3,14 +3,13 @@ from pybuilder.core import use_plugin, init
 use_plugin("python.core")
 use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
-use_plugin("python.flake8")
 use_plugin("python.distutils")
 
 
 name = "viihdepython"
-default_task = ["install_dependencies","publish"]
+default_task = "publish"
 
 
 @init
 def set_properties(project):
-    project.depends_on_requirements("requirements.txt")
+    pass
